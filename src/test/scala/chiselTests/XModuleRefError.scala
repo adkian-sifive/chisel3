a17532a749c97aa1c7bc7d49d0dd5f121a3fe043
Aditya Naik <aditya.naik@sifive.com>
2021-09-30 16:21:19 -0700

    Add XModuleRefError Chisel test

---
+++ src/test/scala/chiselTests/XModuleRefError.scala
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Tests which demonstrate the error conditions and the resulting error
+//   messages for cross-module value reads and writes.
+// Elaboration of OuterAssignExample and OuterReadExample is expected to fail
+
+package chiselTests
+
+import chisel3._
+import chisel3.stage.ChiselStage
+import chisel3.testers.BasicTester
+
+class InnerExample extends Module {
+  val a, b, c  = IO(Input(Bool()))
+  val d, e, f  = IO(Input(Bool()))
+  val foo, bar = IO(Input(UInt(8.W)))
+  val out      = IO(Output(UInt(8.W)))
+
+  val myReg = RegInit(0.U(8.W))
+  out := myReg
+
+  when (a && b && c) {
+    myReg := foo
+  }
+  when (d && e && f) {
+    myReg := bar
+  }
+}
+
+
+class OuterAssignExample extends Module {
+  val a, b, c  = IO(Input(Bool()))
+  val d, e, f  = IO(Input(Bool()))
+  val foo, bar = IO(Input(UInt(8.W)))
+  val out      = IO(Output(UInt(8.W)))
+
+  val myReg = RegInit(0.U(8.W))
+  out := myReg
+
+  when (a && b && c) {
+    myReg := foo
+  }
+  when (d && e && f) {
+    myReg := bar
+  }
+  
+  val inner = Module(new InnerExample())
+  
+  inner.myReg := false.B
+}
+
+class OuterReadExample extends Module {
+  val a, b, c  = IO(Input(Bool()))
+  val d, e, f  = IO(Input(Bool()))
+  val foo, bar = IO(Input(UInt(8.W)))
+  val out      = IO(Output(UInt(8.W)))
+
+  val myReg = RegInit(0.U(8.W))
+  out := myReg
+
+  when (a && b && c) {
+    myReg := foo
+  }
+  when (d && e && f) {
+    myReg := bar
+  }
+  
+  val inner = Module(new InnerExample())
+  
+  myReg := inner.myReg
+}
+
+class XModuleRefError extends ChiselPropSpec {
+
+  property("Assign elaboration should fail") {
+    ChiselStage.elaborate { new OuterAssignExample }
+  }
+
+  property("Read elaboration should fail") {
+    ChiselStage.elaborate { new OuterReadExample }
+  }
+}
