bd8102dfadf53f0aa7d04d690679a8457fda6bf7
Aditya Naik <aditya.naik@sifive.com>
2021-10-01 11:03:41 -0700

    Minimal example and emit with full stack trace

--- src/test/scala/chiselTests/XModuleRefError.scala
+++ src/test/scala/chiselTests/XModuleRefError.scala
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
-// Tests which demonstrate the error conditions and the resulting error
-//   messages for cross-module value reads and writes.
-// Elaboration of OuterAssignExample and OuterReadExample is expected;
-//   the error messages are intercepted and printed
+/**
+  * Tests which demonstrate the error conditions and the resulting error
+  *   messages for cross-module value reads and writes.
+  *
+  * Elaboration of OuterAssignExample and OuterReadExample is expected;
+  *   the error messages are intercepted and printed
+  */
 
 package chiselTests
 
@@ -12,73 +15,33 @@ import chisel3.stage.ChiselStage
 import chisel3.testers.BasicTester
 
 class InnerExample extends Module {
-  val a, b, c  = IO(Input(Bool()))
-  val d, e, f  = IO(Input(Bool()))
-  val foo, bar = IO(Input(UInt(8.W)))
-  val out      = IO(Output(UInt(8.W)))
-
   val myReg = RegInit(0.U(8.W))
-  out := myReg
-
-  when (a && b && c) {
-    myReg := foo
-  }
-  when (d && e && f) {
-    myReg := bar
-  }
 }
 
 
 class OuterAssignExample extends Module {
-  val a, b, c  = IO(Input(Bool()))
-  val d, e, f  = IO(Input(Bool()))
-  val foo, bar = IO(Input(UInt(8.W)))
-  val out      = IO(Output(UInt(8.W)))
-
-  val myReg = RegInit(0.U(8.W))
-  out := myReg
-
-  when (a && b && c) {
-    myReg := foo
-  }
-  when (d && e && f) {
-    myReg := bar
-  }
-  
   val inner = Module(new InnerExample())
-  
-  inner.myReg := false.B
+  inner.myReg := false.B // ERROR
 }
 
 class OuterReadExample extends Module {
-  val a, b, c  = IO(Input(Bool()))
-  val d, e, f  = IO(Input(Bool()))
-  val foo, bar = IO(Input(UInt(8.W)))
-  val out      = IO(Output(UInt(8.W)))
-
   val myReg = RegInit(0.U(8.W))
-  out := myReg
-
-  when (a && b && c) {
-    myReg := foo
-  }
-  when (d && e && f) {
-    myReg := bar
-  }
-  
   val inner = Module(new InnerExample())
-  
-  myReg := inner.myReg
+
+  myReg := inner.myReg // ERROR
 }
 
 class XModuleRefError extends ChiselPropSpec {
   property("Assign elaboration should fail") {
-    val errout = intercept[Exception] {ChiselStage.elaborate { new OuterAssignExample }}
-    println(errout)
+    // ChiselStage.elaborate { new OuterAssignExample }
+    (new ChiselStage).emitVerilog(new OuterAssignExample, Array("--full-stacktrace"))
+    // val errout = intercept[Exception] {ChiselStage.elaborate { new OuterAssignExample }}
+    // println(errout)
   }
 
   property("Read elaboration should fail") {
-    val errout = intercept[Exception] {ChiselStage.elaborate { new OuterReadExample }}
-    println(errout)
+    (new ChiselStage).emitVerilog(new OuterReadExample, Array("--full-stacktrace"))
+    // val errout = intercept[Exception] {ChiselStage.elaborate { new OuterReadExample }}
+    // println(errout)
   }
 }
